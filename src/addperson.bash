ed433c3f4266ddb48d3d6d5ad79aa5f9dc664cd7
George Taveras <george.taveras1231@gmail.com>
2016-09-28 09:19:16 -0400

    Add argument check to `addperson`

--- src/addperson.bash
+++ src/addperson.bash
@@ -1,5 +1,10 @@
 addperson() {
   ensure_initialized
 
+  if [[ -z $@ ]]; then
+    echo "$(basename $0) addperson identifier1 [identifierN...]"
+    exit 1
+  fi
+
   gpg --export $@ | gpg --homedir $project_root/.gitgpg --import
 }
